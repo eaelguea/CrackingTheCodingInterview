f1a91de7af079d2407eefb300b37ea95fc492fe4
eaelguea <31897884+eaelguea@users.noreply.github.com>
2018-04-17 20:39:38 -0600

    Update Question_1_7.java

--- Chapter_1/Question_1_7.java
+++ Chapter_1/Question_1_7.java
@@ -1,4 +1,8 @@
-/*
+/**
+ * Chapter 1 questions
+ * Problems from Cracking the Coding Interview
+ * @author Eric Elguea
+ *
  * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, 
  * write a method to rotate the image by 90 degrees. Can you do this in place?
  */
